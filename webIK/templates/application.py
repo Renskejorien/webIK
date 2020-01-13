c108b9bf1364ddf6fb8a77e6db8da507e19db102
Wouter Knibbe <wouterventje@gmail.com>
2020-01-13 15:55:50 +0000

    website template done

--- webIK/templates/application.py
+++ webIK/templates/application.py
@@ -30,61 +30,35 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///spel.db")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     """Shows homescreen"""
 
-    """TODO"""
+    return render_template("index.html")
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     """Buy shares of stock"""
 
-    if request.method == "POST":
-        print("wauw")
-
-    else:
-        print("niet wauw")
+    return redirect("/")
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
 
-    purchases = db.execute(
-                "SELECT symbol, price, number, created_at FROM purchases WHERE userid = :userid ORDER BY created_at DESC",
-                userid=session["user_id"])
-
-    return render_template("history.html", purchases=purchases)
+    return redirect("/")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
+    return redirect("/")
 
 
 @app.route("/logout")
@@ -103,46 +77,26 @@ def logout():
 def quote():
     """Get stock quote."""
 
-    print("lol")
+    return redirect("/")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
 
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Query database for username
-        taken = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
-
-
-        # Insert user into users and store a hashed version of user's password
-        db.execute("INSERT INTO users VALUES (NULL, :username, :password, 10000)",
-                    username=request.form.get("username"),
-                    password=generate_password_hash(request.form.get("password")))
-
-        # Redirect user to login page
-        return redirect("/login")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("register.html")
+    return redirect("/")
 
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
     """"""
+    return redirect("/")
 
 def errorhandler(e):
     """Handle error"""
     if not isinstance(e, HTTPException):
         e = InternalServerError()
-    return print("poep")
+    return print(e.name, e.code)
 
 
 # Listen for errors
