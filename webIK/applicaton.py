2940a8286155e57923a27890f3ea7cff134e3726
Wouter Knibbe <wouterventje@gmail.com>
2020-01-13 11:04:02 +0000

    webIK

---
+++ webIK/applicaton.py
@@ -0,0 +1,173 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Shows homescreen"""
+
+    """TODO"""
+
+@app.route("/add_funds", methods=["GET", "POST"])
+@login_required
+def add_funds():
+    """Add funds to your account"""
+
+    if request.method == "POST":
+
+        # Get requested amount cash should be increased with
+        amount = float(request.form.get("amount"))
+
+        # Increase cash by amount
+        db.execute("UPDATE users SET cash = cash + :amount WHERE id = :user_id",
+                    user_id=session["user_id"],
+                    amount=amount)
+
+        # Return to homepage
+        return redirect("/")
+
+    else:
+        return render_template("add_funds.html")
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+
+    if request.method == "POST":
+        print("wauw")
+
+    else:
+        print("niet wauw")
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+
+    purchases = db.execute(
+                "SELECT symbol, price, number, created_at FROM purchases WHERE userid = :userid ORDER BY created_at DESC",
+                userid=session["user_id"])
+
+    return render_template("history.html", purchases=purchases)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+
+    print("lol")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Query database for username
+        taken = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+
+        # Insert user into users and store a hashed version of user's password
+        db.execute("INSERT INTO users VALUES (NULL, :username, :password, 10000)",
+                    username=request.form.get("username"),
+                    password=generate_password_hash(request.form.get("password")))
+
+        # Redirect user to login page
+        return redirect("/login")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """"""
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return print("poep")
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
