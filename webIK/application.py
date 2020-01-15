19180f8bc6aaea3cf963e4efca5710ef541f614d
Renske de Leeuw <renske19@gmail.com>
2020-01-15 10:42:56 +0000

    button works

--- webIK/application.py
+++ webIK/application.py
@@ -40,21 +40,24 @@ def homescreen():
     if request.method == "GET":
         return render_template("homescreen.html")
 
-@app.route("/newroom", methods=["POST"])
-@login_required
+@app.route("/newroom", methods=["GET", "POST"])
 def newroom():
     """Makes new room number"""
-    username = request.form.get("username")
-    # Check if username is unique
-    result = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
-    if result:
-        return apology("This username already exists")
-    roomnumber = random.randint(00000, 99999)
-    exists = db.execute("SELECT roomnumber FROM rooms WHERE roomnumber = :roomnumber ", roomnumber=roomnumber)
-    while exists:
-            roomnumber = random.randint(00000, 99999)
-    db.execute("INSERT INTO users (roomnumber, username) VALUES(:roomnumber, :username)", username=username, roomnumber=roomnumber)
-    return redirect("/board", roomnumber, username, )
+    if request.method == "POST":
+        print("hoi")
+        username = request.form.get("username")
+        # Check if username is unique
+        result = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+        if result:
+            return apology("This username already exists")
+        roomnumber = random.randint(00000, 99999)
+        exists = db.execute("SELECT roomnumber FROM rooms WHERE roomnumber = :roomnumber ", roomnumber=roomnumber)
+        while exists:
+                roomnumber = random.randint(00000, 99999)
+        db.execute("INSERT INTO users (roomnumber, username, place, turn) VALUES(:roomnumber, :username, :place, :turn)", username=username, roomnumber=roomnumber, place=1, turn=1)
+        return redirect("/board", roomnumber, username)
+    else:
+        return render_template("newroom.html")
 
 @app.route("/existing", methods=["POST"])
 @login_required
@@ -62,8 +65,10 @@ def existing():
     """Add player to room"""
     username = request.form.get("username")
     roomnumber = request.form.get("roomnumber")
-    db.execute("INSERT INTO users (roomnumber, username) VALUES(:roomnumber, :username)", username=username, roomnumber=roomnumber)
-    return render_template("board.html")
+    in_room = db.execute("SELECT username FROM rooms WHERE roomnumber = :roomnumber", roomnumber=roomnumber)
+    turn = len(in_room) + 1
+    db.execute("INSERT INTO users (roomnumber, username, place, turn) VALUES(:roomnumber, :username, :place, :turn)", username=username, roomnumber=roomnumber, place=1, turn=turn)
+    return redirect("/board", roomnumber, username)
 
 @app.route("/questions", methods=["GET", "POST"])
 @login_required
