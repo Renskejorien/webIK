6716047d8548cc6efe2c7b556ea5b692cef14066
Thomas Koene <thomaskoene@gmail.com>
2020-01-23 11:40:00 +0000

    nog meer opmaak in styles.css

--- webIK/application.py
+++ webIK/application.py
@@ -33,7 +33,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-socketio = SocketIO(app, ping_timeout=10)
+# socketio = SocketIO(app, ping_timeout=10)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///spel.db")
