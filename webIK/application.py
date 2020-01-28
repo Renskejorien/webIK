495080ef5fea885a99c5dd379a6880393959a3cc
Renske de Leeuw <renske19@gmail.com>
2020-01-28 11:15:05 +0000

    joee

--- webIK/application.py
+++ webIK/application.py
@@ -32,7 +32,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///spel.db")
 
